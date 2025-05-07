f75a13834d68ab1b7e240943da0969d304a6f6f1
Endorze <alexyurihallgren@hotmail.com>
2025-05-07 17:39:27 +0200

    Breaktime aka DINNER

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,22 +1,50 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import styles from "./navbar.module.css"
 import logo from "../../assets/carlex.png"
 
+const Menu = () => {
+    return (
+        <>
+            <p><a href="/">Home</a></p>
+            <p><a href="/">Home</a></p>
+            <p><a href="/">Home</a></p>
+            <p><a href="/">Home</a></p>
+            <p><a href="/">Home</a></p>
+        </>
+    )
+}
+
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <div className={styles.gptNavbar}>
             <div className={styles.gptNavbarLinks}>
                 <div className={styles.gptNavbarLinksLogo}>
-                    <img src={logo} alt="logo"/> 
+                    <img src={logo} alt="logo" />
                 </div>
                 <div className={styles.gptNavbarLinksContainer}>
-                    <p><a href="/">Home</a></p>
-                    <p><a href="/">Home</a></p>
-                    <p><a href="/">Home</a></p>
-                    <p><a href="/">Home</a></p>
-                    <p><a href="/">Home</a></p>
+                    <Menu />
                 </div>
+            </div>
+            <div className={styles.gptNavbarSign}>
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+            </div>
+            <div className={styles.gptNavbarMenu}>
+                {toggleMenu ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} /> : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+                {toggleMenu && (
+                    <div className={`${styles.gptNavbarMenuContainer} scale-up-center`}>
+                        <div className={styles.gptNavbarMenuContainerLinks}>
+                            <Menu />
+                        </div>
+                        <div className={styles.gptNavbarMenuContainerLinksSign}>
+                            <p>Sign in</p>
+                            <button type="button">Sign up</button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
