368c080bdc6375f1b83b743809307366c4a284c7
Endorze <alexyurihallgren@hotmail.com>
2025-05-08 14:22:56 +0200

    feat: Brand section finished

--- src/components/brand/brand.tsx
+++ src/components/brand/brand.tsx
@@ -1,11 +1,25 @@
 import type { FC } from "react";
 import styles from "./brand.module.css"
-
+import { atlassian, google, slack, shopify, dropbox } from "./imports";
 
 const Brand: FC = () => {
     return (
-        <div className={styles.brand}>
-
+        <div className={`${styles.brand} sectionPadding`}>
+            <div>
+                <img src={google}/>
+            </div>
+            <div>
+                <img src={slack}/>
+            </div>
+            <div>
+                <img src={atlassian}/>
+            </div>
+            <div>
+                <img src={shopify}/>
+            </div>
+            <div>
+                <img src={dropbox}/>
+            </div>
         </div>
     )
 }
