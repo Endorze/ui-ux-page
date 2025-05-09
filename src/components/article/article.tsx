6696daec0ff215f516841b37bef598377466790a
Endorze <alexyurihallgren@hotmail.com>
2025-05-09 14:54:03 +0200

    feat: Added a blog section, fully responsive

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -1,11 +1,25 @@
 import type { FC } from "react";
 import styles from "./article.module.css"
 
+type Props = {
+    imgUrl: string,
+    date: string,
+    title: string,
+}
 
-const Article: FC = () => {
+const Article: FC<Props> = ({imgUrl, date, title}) => {
     return (
         <div className={styles.article}>
-
+            <div className={styles.articleImage}>
+                <img src={imgUrl} alt="blog image"/>
+            </div>
+            <div className={styles.articleContainer}>
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
         </div>
     )
 }
